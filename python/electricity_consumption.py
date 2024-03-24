9a2096880eef28f6835f512b5fe01914d790244d
JanTubeeckx <jan.tubeeckx@elmos.be>
2024-03-24 14:14:44 +0100

    Refactor electricity_consumption script

--- python/electricity_consumption.py
+++ python/electricity_consumption.py
@@ -13,25 +13,42 @@ import matplotlib.pyplot as plt
 load_dotenv()
 
 # Instantiate an InfluxDB client
-client = InfluxDBClient3(
+client1 = InfluxDBClient3(
     token=os.getenv("ACCESS_TOKEN"),
     host=os.getenv("DB_HOST"),
     database=os.getenv("DB_NAME"))
 
+client2 = InfluxDBClient3(
+    token=os.getenv("ACCESS_TOKEN"),
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME_PROD"))
+
 # Execute query to retrieve all time series
-table = client.query(
-  '''SELECT *
-    FROM meter_reading
-    WHERE time >= now() - INTERVAL '6 hours'
-    ORDER BY time'''
+interval = 12
+consumption = client1.query(
+  "SELECT time, current_consumption FROM meter_reading WHERE time >= now() - INTERVAL '" 
+  + str(interval) + " hours' ORDER BY time"
 )
 
-client.close()
+client1.close()
 
-dataframe = table.to_pandas()
+production = client2.query(
+  "SELECT time, current_power FROM inverter_reading WHERE time >= now() - INTERVAL '" 
+  + str(interval) + " hours' ORDER BY time"
+)
 
-dataframe.plot.area(x="time", y="current_consumption")
+client2.close()
+
+dataframe1 = consumption.to_pandas()
+dataframe2 = production.to_pandas()
+
+plt.figure(figsize=(32, 16), dpi=150) 
+
+# dataframe1.plot.area(x='time', y='current_consumption', color="orange")
+# dataframe2.plot.area(x='time', y='current_power', color="green")
+
+# Plot both consumption and prodution datafames
+first = dataframe1.plot(x='time', color='orange')
+dataframe2.plot(x='time', ax=first, color='green')
 
 plt.show()
-
-# print(dataframe)
