c6e14b0cfc1733e92ed83028358a5b16bdcd6e29
JanTubeeckx <jan.tubeeckx@elmos.be>
2024-04-07 20:54:13 +0200

    Create project

--- client/poc_hems/poc_hems/ContentView.swift
+++ client/poc_hems/poc_hems/ContentView.swift
@@ -6,17 +6,44 @@
 //
 
 import SwiftUI
+import Charts
+
+struct MonthlyHoursOfSunshine: Identifiable {
+    var id: Int
+    var city: String
+    var date: Date
+    var hoursOfSunshine: Double
+
+
+    init(id: Int, city: String, month: Int, hoursOfSunshine: Double) {
+        let calendar = Calendar.autoupdatingCurrent
+        self.id = id
+        self.city = city
+        self.date = calendar.date(from: DateComponents(year: 2020, month: month))!
+        self.hoursOfSunshine = hoursOfSunshine
+    }
+}
+
+
+var data: [MonthlyHoursOfSunshine] = [
+    MonthlyHoursOfSunshine(id: 1, city: "Seattle", month: 1, hoursOfSunshine: 74),
+    MonthlyHoursOfSunshine(id: 2,city: "Cupertino", month: 1, hoursOfSunshine: 196),
+    // ...
+    MonthlyHoursOfSunshine(id: 3,city: "Seattle", month: 12, hoursOfSunshine: 62),
+    MonthlyHoursOfSunshine(id: 4,city: "Cupertino", month: 12, hoursOfSunshine: 199)
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Chart(data) {
+                LineMark(
+                    x: .value("Month", $0.date),
+                    y: .value("Hours of Sunshine", $0.hoursOfSunshine)
+                )
+                .foregroundStyle(by: .value("City", $0.city))
+            }
     }
+    
 }
 
 #Preview {
