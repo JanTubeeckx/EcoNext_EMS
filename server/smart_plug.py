0ee4d3ca0fbe78bcc7e869bc970bffe63f5d1da9
JanTubeeckx <jan.tubeeckx@elmos.be>
2024-05-09 22:40:03 +0200

    Add smart plug password to .env file

--- server/smart_plug.py
+++ server/smart_plug.py
@@ -1,6 +1,11 @@
+from dotenv import load_dotenv
 from PyP100 import PyP100
 from datetime import datetime, timedelta
 from xgboost_forecast import prediction
+import os
+
+# Loading variables from .env file
+load_dotenv()
 
 current_hour = datetime.now().hour
 tommorrow = (datetime.now() + timedelta(days=1)).day
@@ -10,17 +15,16 @@ tommorrow = (datetime.now() + timedelta(days=1)).day
 start_time = prediction.loc[(prediction['final_prediction'] >= 0) & 
                             (prediction.index.day == tommorrow)].tail(1).index.hour[0]
 
-print()
+smart_plug_1 = PyP100.P100("192.168.1.47", "jan.tubeeckx@hotmail.com", os.getenv("SMART_PLUG_PASSWORD"))
 
-p100 = PyP100.P100("192.168.1.47", "jan.tubeeckx@hotmail.com", "Mezta840!")
-
+print(tommorrow)
 print(current_hour)
 print(start_time)
 
 while True:
   if (current_hour == (start_time - 1)):
-      p100.turnOn()
-      p100.turnOffWithDelay(20)
+      smart_plug_1.turnOn()
+      smart_plug_1.turnOffWithDelay(20)
   else:
-    p100.turnOff()
+    smart_plug_1.turnOff()
     
