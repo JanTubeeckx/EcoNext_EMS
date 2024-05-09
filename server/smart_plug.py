2c0b3ef67c3d7672035c8e4d93e5481f17ea87e1
JanTubeeckx <jan.tubeeckx@elmos.be>
2024-05-09 22:59:18 +0200

    Rework smart plug script

--- server/smart_plug.py
+++ server/smart_plug.py
@@ -9,17 +9,11 @@ load_dotenv()
 
 current_hour = datetime.now().hour
 tommorrow = (datetime.now() + timedelta(days=1)).day
-# Set starting time at first hour of the day with prediction above 900W PV power
-# start_time = prediction.loc[(prediction['final_prediction'] > 900) & 
-#                             (prediction.index.day == tommorrow)].head(1).index.hour[0]
-start_time = prediction.loc[(prediction['final_prediction'] >= 0) & 
+# Activate device when PV power reaches 800 Watts
+start_time = prediction.loc[(prediction['final_prediction'] >= 800) & 
                             (prediction.index.day == tommorrow)].tail(1).index.hour[0]
 
 smart_plug_1 = PyP100.P100("192.168.1.47", "jan.tubeeckx@hotmail.com", os.getenv("SMART_PLUG_PASSWORD"))
-
-print(tommorrow)
-print(current_hour)
-print(start_time)
 
 while True:
   if (current_hour == (start_time - 1)):
