15b935067d0cd174cd845f8553f1159c7c711ab5
JanTubeeckx <jan.tubeeckx@elmos.be>
2024-05-11 14:10:56 +0200

    Symplify smart plug script

--- server/smart_plug.py
+++ server/smart_plug.py
@@ -8,11 +8,11 @@ import os
 # Loading variables from .env file
 load_dotenv()
 
-# Switch to Raspberry Pi version
-isRPi = False
+# # Switch to Raspberry Pi version
+# isRPi = False
 
 # Define moments of time
-current_hour = (datetime.now() + timedelta(hours=1)).hour if isRPi else datetime.now().hour
+# current_hour = (datetime.now() + timedelta(hours=1)).hour if isRPi else datetime.now().hour
 tommorrow = (datetime.now() + timedelta(days=1)).day
 
 # Connect to smart plug 1
@@ -25,18 +25,17 @@ prediction = predictpvpower()
 start_time = prediction.loc[(prediction['final_prediction'] > 800) & 
                             (prediction.index.day == tommorrow)].head(1).index.hour[0]
 print(start_time)
-delay = (start_time + timedelta(hours=1)) * 3600
+delay = (start_time + 1) * 3600
 duration = 3 * 3600
 
 while True:
-  if (current_hour == (start_time + 4)):
-      try:
-        time.sleep(delay)
-        smart_plug_1.turnOn()
-        time.sleep(30)
-        smart_plug_1.turnOff()
-        os.system('clear||cls')
-        quit()
-      except:
-        quit()
+  try:
+    time.sleep(10)
+    smart_plug_1.turnOn()
+    time.sleep(30)
+    smart_plug_1.turnOff()
+    os.system('clear||cls')
+    quit()
+  except:
+    quit()
     
